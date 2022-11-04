19fa1630ca11c2a2ca29c9dbd1f68f31865e81fd
Low Kok Wei <98858885+lowkw@users.noreply.github.com>
2022-11-04 17:06:55 +0800

    Add files via upload

---
+++ DeleteMember.php
@@ -0,0 +1,90 @@
+<?php
+// Team      : ALE (Atit, Ellena, Low)
+// Developer : Low, Kok Wei
+// Date      : Oct 2022
+?>
+<?php
+// Initialize the session
+session_start();
+ 
+// Check if the user is logged in, if not then redirect user to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+?>
+<?php
+include_once('include/db_connect_acme_pdo.php');
+
+// Proces delete operation after confirmation
+if (isset($_POST["Email"]) && !empty($_POST["Email"])) {
+    $email = $_POST["Email"];
+
+    try {
+        $sql = "DELETE FROM Member WHERE Email=?";
+        if ($stmt = $pdo->prepare($sql)) {
+            if ($stmt->execute([$email])) {
+                // Record deleted successfully. Redirect to landing page
+                header("location: MemberTable.php");
+                exit();
+            } else {
+                echo "Something went wrong. Please try again later.";
+            }
+        }
+        unset($stmt);
+    } catch (PDOException $e) {
+        die("ERROR: Could not able to execute $sql. " . $e->getMessage());
+    }
+    include_once('include/db_close_pdo.php');
+} else {
+    if (empty(trim($_GET["Email"]))) {
+        //Redirect to landing page					
+        header("location: MemberTable.php");
+        exit();
+    }
+}
+?>
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="Low Kok Wei">    
+        <title>Delete Member</title>
+
+        <link href="./bootstrap/css/bootstrap.min.css" rel="stylesheet">        
+        <!-- Custom styles for this template -->
+        <link href="navbar-top-fixed.css" rel="stylesheet">
+    </head>
+
+    <body>    		    
+        <?php
+        include_once('include/navbar.php');
+        include_once('include/db_connect_acme_pdo.php');
+        ?>
+        <main class="container">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h2 class="mt-5 mb-3">Delete Member</h2>                    
+                        <!-- Avoid PHP_SELF exploit -->
+                        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+                            <div class="alert alert-danger">
+								<!-- Display which member we are about to delete in a readonly field. -->
+                                <input type="text" readonly name="Email" value="<?php echo trim($_GET["Email"]); ?>"/>
+                                <p>Are you sure you want to delete this member record?</p>
+                                <p>
+                                    <input type="submit" value="Yes" class="btn btn-danger">
+                                    <!-- "No" button to redirect to landing page -->
+                                    <a href="MemberTable.php" class="btn btn-secondary">No</a>
+                                </p>
+                            </div>
+                        </form>
+                    </div>
+                </div>        
+            </div>    
+        </main> 
+        <script src="./bootstrap/js/bootstrap.bundle.min.js"></script>  
+    </body>
+</html>
---
+++ MemberTable.php
@@ -0,0 +1,106 @@
+<?php
+// Team      : ALE (Atit, Ellena, Low)
+// Developer : Low, Kok Wei
+// Date      : Oct 2022
+?>
+<?php
+// Initialize the session
+session_start();
+ 
+// Check if the user is logged in, if not then redirect user to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Low Kok Wei">        
+	<title>Member</title>
+
+	<link href="./bootstrap/css/bootstrap.min.css" rel="stylesheet">       
+	<!-- Custom styles for this template -->
+    <link href="navbar-top-fixed.css" rel="stylesheet">
+</head>
+
+<body>    	
+<?php
+	include_once('include/navbar.php');
+	include_once('include/db_connect_acme_pdo.php');	
+?>	
+  
+	<main class="container">
+	  <div class="bg-light p-3 rounded">
+		<h1>Member</h1>				
+			<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" class="d-flex" role="search" method="post">
+                <input class="form-control ms-2 me-2" type="search" placeholder="Member Email Search" aria-label="Search" name="Email">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+									
+<?php		
+			//Processing member after self-call is submitted
+			if (isset($_POST['Email'])) {
+				if(!empty($_POST['Email'])) {
+					$email = $_POST['Email'];
+					
+					try {
+						$sql = "SELECT * FROM Member WHERE Email = '$email'";          
+						$result = $pdo->query($sql);			
+						if ($result->rowCount()>0){								
+							$row = $result->fetch();
+?>						
+							<table class="table table-bordered table-striped mt-2 ms-2">
+                                    <thead>
+                                        <tr>                                        
+                                            <th>Id</th>
+                                            <th>First Name</th>                                        
+                                            <th>Last Name</th>
+                                            <th>Email</th>
+                                            <th>Monthly Newsletter</th>
+											<th>Breaking News</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>                                        
+										<tr>
+                                            <td><?= $row['Id'] ?></td>
+                                            <td><?= $row['FirstName'] ?></td>
+                                            <td><?= $row['LastName'] ?></td>
+                                            <td><?= $row['Email'] ?></td>
+											<td><?= $row['MonthlyNewsletter'] ?></td>
+											<td><?= $row['BreakingNews'] ?></td>
+                                            <td>                                                                       
+                                               <!-- Button to delete this member -->
+                                                <a href="DeleteMember.php?&Email=<?= $row['Email'] ?>" class="btn btn-secondary">Delete</a>
+                                            </td>
+                                        </tr>			
+                                        
+                                    </tbody>                        
+							</table>
+<?php                                        
+                            unset($result);                            
+						}
+						else{			
+							echo "<p class=mt-2>Member Email address not found ! </p>";						
+						}
+						
+					} catch(PDOException $e) {
+						die("ERROR: Could not able to execute $sql. " . $e->getMessage());
+					}
+				}
+			}
+			include_once('include/db_close_pdo.php');		  					
+?>
+		<div class="btn-group mt-2 ms-2" role="group" aria-label="Basic outlined">		  
+		  <a href="#" class="btn btn-outline-primary">All</a>
+		  <a href="#" class="btn btn-outline-primary">Monthly Newsletter</a>
+		  <a href="#" class="btn btn-outline-primary">Breaking News</a>
+		</div>
+	</div>
+	</main>  
+    <script src="./bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
